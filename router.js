83ddb4a7335bcf6a559442150e991544498de4d9
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-11-22 19:59:11 +0300

    Add comments to router.js

--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const db = require("./queries");
 
-router.get("/", db.getUsers);
-router.get("/:id", db.getUserById);
-router.post("/", db.createUser);
-router.put("/:id", db.updateUser);
-router.delete("/:id", db.deleteUser);
+// Routing for "/"
+router.get("/", db.getUsers); // localhost:8080
+router.get("/:id", db.getUserById); // localhost:8080/:id
+router.post("/", db.createUser); // localhost:8080
+router.put("/:id", db.updateUser); // localhost:8080/:id
+router.delete("/:id", db.deleteUser); // localhost:8080/:id
 
 module.exports = router;
